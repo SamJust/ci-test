c0a594cdb3de5011ebf457bf0ae8df646d0c3a83
Viktor Bezrodnyi <viktor.bezrodnyi@gmail.com>
2020-01-28 20:33:25 +0200

    controller test

--- test.spec.js
+++ test.spec.js
@@ -1,5 +1,12 @@
+const {get} = require('./controllers');
+
 describe('this is a test', () => {
-	it('should pass', () => {
-		expect(true).toEqual(true);
+	it('should pass', async () => {
+		const resMock = {
+			send: jest.fn()
+		};
+
+		await get({}, resMock);
+		expect(resMock.send).toHaveBeenCalledWith('HUMANS MUST DIE!');
 	});
 });
